7cb1d4cadeb78efbb1a795ac0a498eb3ca1b6746
fimkg <kgaugelob82@gmail.com>
2020-11-17 09:26:13 +0200

    Pro-active measures

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,22 +199,22 @@
     aos_init();
   });
 
-  // Maps
-  function initMap() {
+//   // Maps
+//   function initMap() {
 
-    var location = new google.maps.LatLng(50.0875726, 14.4189987);
+//     var location = new google.maps.LatLng(50.0875726, 14.4189987);
 
-    var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-        center: location,
-        zoom: 16,
-        panControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+//     var mapCanvas = document.getElementById('map');
+//     var mapOptions = {
+//         center: location,
+//         zoom: 16,
+//         panControl: false,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     }
+//     var map = new google.maps.Map(mapCanvas, mapOptions);
 
-}
+// }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+// google.maps.event.addDomListener(window, 'load', initMap);
 
 })(jQuery);
